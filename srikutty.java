8712e30c4a0bf25e3e1d5b587079273387355038
srikavi33 <47377676+srikavi33@users.noreply.github.com>
2019-02-06 14:29:13 +0530

    Create srikutty.java

---
+++ srikutty.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class java
+{
+public static void Main(String[] args)
+{
+int number=2;
+if(number > 0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+else{
+System.out.println("Invalid print");
+}
+}
+}
