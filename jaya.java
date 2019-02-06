d75da1c55ab3be7260b386bb5248e29e0900934f
srikavi33 <47377676+srikavi33@users.noreply.github.com>
2019-02-06 14:17:33 +0530

    Create jaya.java

---
+++ jaya.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class java
+{
+public static void main(String[] args)
+{
+int number=2;
+if (number >0)
+{
+if(number%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+else{
+System.out.println("invalid input");
+}
+}
+}
