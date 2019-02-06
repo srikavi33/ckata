7b063c4f0eec2e901cfb1a18824215e0be60da00
srikavi33 <47377676+srikavi33@users.noreply.github.com>
2019-02-06 14:09:58 +0530

    Create jai.java

---
+++ jai.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+public class Main
+{
+public static void main(string[] args)
+{
+int number=2;
+if(r%2==0)
+{
+system.out.println("even");
+}
+else
+{
+system.out.println("odd");
+}
+}
+else{
+system.out.println("Invalid input");
+}
+}
+}
